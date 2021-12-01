aaaa083975cfdac542ff39b3ef4c3f6b4e6f10c7
sophie <tansophie@qq.com>
2021-12-01 21:39:58 +0800

    fix：isJSON方法

--- xmcet-client/src/utils/mt.js
+++ xmcet-client/src/utils/mt.js
@@ -222,3 +222,18 @@ export function download(path, params) {
     querystring.stringify(params)
   window.location.href = url
 }
+
+export function isJSON(str) {
+  if (typeof str === 'string') {
+    try {
+      var obj = JSON.parse(str)
+      if (typeof obj === 'object' && obj) {
+        return true
+      } else {
+        return false
+      }
+    } catch (e) {
+      return false
+    }
+  }
+}
