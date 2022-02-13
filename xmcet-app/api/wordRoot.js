7fc1b132fbfc154b4734bf54b9da5c282fd59384
Sophie <sophie@SophiedeMacBook-Pro.local>
2022-02-13 23:16:36 +0800

    dev: 词根词缀闯关页面

--- xmcet-app/api/wordRoot.js
+++ xmcet-app/api/wordRoot.js
@@ -8,6 +8,14 @@ export const getList = (data) => {
 	})
 }
 
+export const getScheduleList = (data) => {
+	return apiResquest({
+		url: 'wordRoot/getScheduleList',
+		method: 'GET',
+		data
+	})
+}
+
 export const getBasicInfo = (data) => {
 	return apiResquest({
 		url: 'wordRoot/getBasicInfo',
@@ -19,6 +27,14 @@ export const getBasicInfo = (data) => {
 export const setPlan = (data) => {
 	return apiResquest({
 		url: 'wordRoot/setPlan',
+		method: 'POST',
+		data
+	})
+}
+
+export const updateRecord = (data) => {
+	return apiResquest({
+		url: 'wordRoot/updateRecord',
 		method: 'POST',
 		data
 	})
--- xmcet-app/pages/wordCard/wordCard.vue
+++ xmcet-app/pages/wordCard/wordCard.vue
@@ -2,10 +2,10 @@
 	<view class="container">
 		<span class="close" @click="handleBack">x</span>
 		<view class="header"> 
-			还需学习5，复习5
+			学习{{studyCount}}|复习{{remaindCount}}
 		</view>
 		<view class='content'>
-			<view class="word">   
+			<view class="word">
 				{{word.word}}
 			</view>
 			<view class="mean">
@@ -15,77 +15,118 @@
 				<view class="detail-item" v-for="(detail, index) in word.detail" :key="detail.word">
 					<view class="item-word">{{index+1}}. {{detail.word}}</view>
 					<view class="item-translare">{{detail.translate}}</view>
-				</view>     
+				</view>
 			</view>
 		</view>
 		<view class="option">
-			<text class="btn btn-left">认识</text>
-			<text class="btn btn-middle">模糊</text>
-			<text class="btn btn-right">不认识</text>
+			<text class="btn btn-left" @click="handleClick(1)">认识</text>
+			<text class="btn btn-middle" @click="handleClick(0)">模糊</text>
+			<!-- <text class="btn btn-right">不认识</text> -->
 		</view>
 	</view>
 </template>
 
 <script>
+	import { getScheduleList, updateRecord } from '@/api/wordRoot'
 	export default {
 		data() {
 			return {
 				word: 
-                    {"id": 2,"word":"2amphi","mean":"两个，两种两个，两种两个，两种",
-                        "detail":[
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                            {
-                            "word": "introduce",
-                            "translate": "提出；介绍；引进；作为…的开头；"
-                            },
-                        ],
+                    {
+						// "id": 2,"word":"2amphi","mean":"两个，两种两个，两种两个，两种",
+                        // "detail":[
+                        //     {
+                        //     "word": "introduce",
+                        //     "translate": "提出；介绍；引进；作为…的开头；"
+                        //     },
+                        // ],
                     },
+				list: [],
+				readyList: [],
+				current: -1
 			}
 		},
+		computed: {
+			remaindCount() {
+				return this.list.filter(item => item.n > 1 && !item.status).length
+			},
+			studyCount() {
+				return this.list.filter(item => item.n == 1 && !item.status).length
+			}
+		},
+		created() {
+			getScheduleList().then(response => {
+				this.list = this.generatePlan(response.data)
+				this.readyList = this.generateReadyList(response.data)
+				this.nextWord()
+			})
+		},
 		methods: {
+			handleClick(status) {
+				updateRecord({
+					id: this.word.id,
+					status: status
+				}).then(response => {
+					this.updateWord(this.word.id, status)
+				})
+			},
+			nextWord() {
+				this.current++
+				if(this.current > this.readyList.length) {
+					this.current = this.current % this.readyList.length
+				}
+				this.word = this.readyList[this.current]
+			},
+			updateWord(id, status) {
+				let index = this.list.findIndex((item)=>{
+					return item.id == id
+				})
+				let target = this.list.find(item => {
+					return item.id == id
+				})
+				if(index > -1) {
+					if(status) {
+						target.n -= 1
+					} else {
+						target.n += 1
+					}
+					this.list.splice(index, 1, target)
+					console.log(this.list)
+					let rindex = this.readyList.findIndex(item => {
+						return item.id == id
+					})
+					let rtarget = this.readyList.find(item => {
+						return item.id = id
+					})
+					if(target.n == 0) {
+						this.readyList.splice(rindex, 1)
+					} else {
+						this.readyList.push(rtarget)
+					}
+				}
+				this.nextWord()
+			},
+			// 生成计划列表
+			generatePlan(array) {
+				let result = [];
+				for (let i = 0; i < array.length; i++) {
+					array[i]['n'] = 1;
+					result.push(array[i]);
+				}
+				return result;
+			},
+			// 生成背词列表
+			generateReadyList(array) {
+				let result = [];
+				for (let i = 0; i < array.length; i++) {
+					array[i]['n'] = 1;
+					if(array[i]['status'] == 0) {
+						result.push(array[i]);
+					}
+				}
+				return result;
+			},
+			// 返回
 			handleBack() {
 				uni.navigateBack({
 					delta: 1
@@ -139,7 +180,7 @@
 		.btn {
 			@btnSize: 70px;
 			border-radius: 10px;	
-			width: @btnSize;
+			width: @btnSize + 10px;
 			height: @btnSize;
 			text-align: center;
 			line-height: @btnSize;
